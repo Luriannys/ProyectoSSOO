7504ecb4609b6bbf1fe2eca1905be70a291a71aa
Luriannys <ljunco@correo.unimet.edu.ve>
2025-09-28 22:25:32 -0400

    View

--- src/proyectossoo/ProyectoSSOO.java
+++ src/proyectossoo/ProyectoSSOO.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package proyectossoo;
+import interfaces.*;
 
 /**
  *
@@ -15,6 +16,9 @@ public class ProyectoSSOO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        View v = new View();
+        v.setVisible(true);
+        
     }
     
 }
